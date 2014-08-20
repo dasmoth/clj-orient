ce79b9b0cabd0c344637272d3dc02470df83d138
Thomas Down <thomas.a.down@gmail.com>
2014-08-20 10:29:53 +0100

    Updated to latest OrientDB libraries

--- project.clj
+++ project.clj
@@ -1,18 +1,17 @@
-(defproject clj-orient "0.5.0"
-  :description "Wrapper for the OrientDB Native APIs. It supports version 1.1 of the APIs."
+(defproject clj-orient "0.5.1-SNAPSHOT"
+  :description "Wrapper for the OrientDB Native APIs. It supports version 1.7 of the APIs."
   :url "https://github.com/eduardoejp/clj-orient"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :plugins [[lein-autodoc "0.9.0"]
-            [lein-swank "1.4.4"]
             [codox "0.6.1"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.orientechnologies/orient-commons "1.1.0"]
-                 [com.orientechnologies/orientdb-client "1.1.0"]
-                 [com.orientechnologies/orientdb-core "1.1.0"]
-                 [com.orientechnologies/orientdb-object "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.orientechnologies/orient-commons "1.7.8"]
+                 [com.orientechnologies/orientdb-client "1.7.8"]
+                 [com.orientechnologies/orientdb-core "1.7.8"]
+                 [com.orientechnologies/orientdb-object "1.7.8"]
                  ]
   :repositories {"sonatype" "https://oss.sonatype.org/content/groups/public/"}
   :autodoc {:name "clj-orient"
